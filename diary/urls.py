465f9b273381aaa952985fafbcf27491c9a50804
lerakuzmina <l3ra131007@gmail.com>
2025-05-19 17:39:13 +0300

    second commit

--- diary/urls.py
+++ diary/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 
 
-from diary.views import HomeView, DiaryEntryList, DiaryEntryCreateView
+from diary.views import HomeView, DiaryEntryList, DiaryEntryCreateView, DiaryEntryUpdateView, DiaryEntryFormDetailView
 
 app_name = 'diary'
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
     path('diarylist/', DiaryEntryList.as_view(), name='diary_list'),
     path('diarycreatelist/', DiaryEntryCreateView.as_view(), name='diary_create'),
+    path('diary/update/<int:pk>', DiaryEntryUpdateView.as_view(), name='diary_update'),
+    path('diary/<int:pk>', DiaryEntryFormDetailView.as_view(), name='diary_detail'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, CreateView
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DetailView, DeleteView
 
 from diary.form import DiaryEntryForm
 from diary.models import DiaryEntry
@@ -39,3 +39,29 @@ class DiaryEntryCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class DiaryEntryUpdateView(LoginRequiredMixin, UpdateView):
+    model = DiaryEntry
+    form_class = DiaryEntryForm
+    template_name = 'diary/diary_form.html'
+    success_url = reverse_lazy('diary:diary_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
+
+
+class DiaryEntryFormDetailView(DetailView):
+    model = DiaryEntry
+    template_name = "diary/diary_detail.html"
+
+
+class DiaryEntryDeleteView(LoginRequiredMixin, DeleteView):
+    model = DiaryEntry
+    success_url = reverse_lazy('diary:diary_list')
+    template_name = "diary/diary_confirm_delete.html"
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
