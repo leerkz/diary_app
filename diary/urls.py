493e3f91cdcfe122478936555c4aaff22e27d72a
lerakuzmina <l3ra131007@gmail.com>
2025-06-05 21:08:01 +0300

    tochno finish 2!!

--- diary/urls.py
+++ diary/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-
-
-
-from diary.views import HomeView, DiaryEntryList, DiaryEntryCreateView, DiaryEntryUpdateView, DiaryEntryFormDetailView, \
-    DiaryEntryDeleteView, DiaryEntrySearchView
+from diary.views import (
+    DiaryEntryList,
+    DiaryEntryCreateView,
+    DiaryEntryUpdateView,
+    DiaryEntryFormDetailView,
+    DiaryEntryDeleteView
+)
 
 app_name = 'diary'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('diarylist/', DiaryEntryList.as_view(), name='diary_list'),
-    path('diarycreatelist/', DiaryEntryCreateView.as_view(), name='diary_create'),
-    path('diary/update/<int:pk>', DiaryEntryUpdateView.as_view(), name='diary_update'),
-    path('diary/<int:pk>', DiaryEntryFormDetailView.as_view(), name='diary_detail'),
-    path('diary/delete/<int:pk>', DiaryEntryDeleteView.as_view(), name='diary_delete'),
-    path('diary/search/', DiaryEntrySearchView.as_view(), name='diary_search'),
+    path('', DiaryEntryList.as_view(), name='diary_list'),
+    path('diary/create/', DiaryEntryCreateView.as_view(), name='diarycreatelist'),
+    path('diary/update/<int:pk>/', DiaryEntryUpdateView.as_view(), name='diary_update'),
+    path('diary/<int:pk>/', DiaryEntryFormDetailView.as_view(), name='diary_detail'),
+    path('diary/delete/<int:pk>/', DiaryEntryDeleteView.as_view(), name='diary_delete'),
 ]
